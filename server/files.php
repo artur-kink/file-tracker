37975b22f5e02238dd56bd00c6c00447fd48c920
artur-kink <artur.kink@gmail.com>
2013-12-27 17:24:49 -0500

    Added datatables plugin for better file list view

--- server/files.php
+++ server/files.php
@@ -1,9 +1,25 @@
 <html>
     <head>
     <title>File List</title>
+    
+    <script type="text/javascript" src="scripts/jquery.1.4.2.min.js"></script>
+    <script type="text/javascript" src="scripts/jquery.dataTables.min.js"></script>
+    <link rel="Stylesheet" type="text/css" href="dataTables.css" />
+
+    <script type="text/javascript">
+        $("document").ready(function () {
+
+            $("#files_table").dataTable({
+                "sPaginationType": "full_numbers",
+                "iDisplayLength": 20,
+                "aLengthMenu": [[20, 50, -1], [20, 50, "All"]]
+            });
+        });
+    </script>
+
     </head>
     <body>
-        <table>
+        <table id="files_table" style="width: 100%;">
             <thead><tr><th>Computer</th><th>File</th><th>Size</th><th>Modified Date</th></tr></thead>
             <tbody>
 <?php
